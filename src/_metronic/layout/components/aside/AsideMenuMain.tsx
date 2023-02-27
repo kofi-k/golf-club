9ed6b3a119ad9febad94668e3db6c3de66e04628
Kofi K <kofivincent026@gmail.com>
2023-02-27 13:07:11 +0000

    AsideMenuItem leading icons

--- src/_metronic/layout/components/aside/AsideMenuMain.tsx
+++ src/_metronic/layout/components/aside/AsideMenuMain.tsx
@@ -11,8 +11,8 @@ export function AsideMenuMain() {
       <AsideMenuItemWithSub to='' title='MAIN NAVIGATION' fontIcon='bi-archive' icon='/media/icons/duotune/general/gen022.svg'>
         <AsideMenuItem to='register' icon='/media/icons/duotune/general/gen055.svg' title='Membership' />
         <AsideMenuItem to='planning' fontIcon='bi-archive' icon='/media/icons/duotune/general/gen014.svg' title='Calendar' />
-        <AsideMenuItem to='tee-sheet' fontIcon='bi-archive' icon='/media/icons/duotune/general/gen022.svg' title='Tee Sheet' />
-        <AsideMenuItem to='gameplay' fontIcon='bi-archive' icon='/media/icons/duotune/general/gen022.svg' title='Gameplay' />
+        <AsideMenuItem to='tee-sheet' fontIcon='bi-archive' icon='/media/icons/duotune/ecommerce/ecm009.svg' title='Tee Sheet' />
+        <AsideMenuItem to='gameplay' fontIcon='bi-archive' icon='/media/icons/duotune/communication/com001.svg' title='Gameplay' />
         <AsideMenuItem to='shop' fontIcon='bi-archive' icon='/media/icons/duotune/ecommerce/ecm001.svg' title='Shop' />
         {/*<AsideMenuItem to='pos' fontIcon='bi-archive' icon='/media/icons/duotune/finance/fin007.svg' title='POS' />*/}
         {/*<AsideMenuItem to='partners' fontIcon='bi-archive' icon='/media/icons/duotune/technology/teh010.svg' title='Partners' />*/}
@@ -28,9 +28,13 @@ export function AsideMenuMain() {
         to='#'
         title='Reports'
         fontIcon='bi-archive'
-        icon='/media/icons/duotune/coding/cod009.svg'
-      >
-        <AsideMenuItem to='/reports/members-report' title='Members report' hasBullet={true} />
+        icon='/media/icons/duotune/general/gen005.svg'>
+        <AsideMenuItem
+            to='/reports/members-report'
+            title='Members report'
+            fontIcon='bi-archive'
+            icon='/media/icons/duotune/communication/com014.svg'
+            hasBullet={false} />
       </AsideMenuItemWithSub>
       <AsideMenuItemWithSub
         to='#'
@@ -38,9 +42,22 @@ export function AsideMenuMain() {
         fontIcon='bi-archive'
         icon='/media/icons/duotune/coding/cod009.svg'
       >
-        <AsideMenuItem to='/setup/course-setup' title='Course Setup' hasBullet={true} />
-        <AsideMenuItem to='/setup/fees' fontIcon='bi-archive' icon='/media/icons/duotune/finance/fin007.svg' title='Fees Setup' />
-        <AsideMenuItem to='/setup/caddies' title='Caddies' hasBullet={true} />
+        <AsideMenuItem
+            to='/setup/course-setup'
+            title='Course Setup'
+            fontIcon='bi-archive'
+            icon='/media/icons/duotune/maps/map003.svg'
+            hasBullet={false} />
+        <AsideMenuItem
+            to='/setup/fees'
+            fontIcon='bi-archive'
+            icon='/media/icons/duotune/finance/fin010.svg' title='Fees Setup' />
+        <AsideMenuItem
+            to='/setup/caddies'
+            title='Caddies'
+            fontIcon='bi-archive'
+            icon='/media/icons/duotune/communication/com013.svg'
+            hasBullet={false} />
 
       </AsideMenuItemWithSub>
 
